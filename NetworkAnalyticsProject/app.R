bbe1e5e964ea9e1e3ebef286f866525a737f9b9c
romulovff <romulo.v.f.f@gmail.com>
2022-03-13 19:29:48 +0000

    Updated project, stats for general done

--- NetworkAnalyticsProject/app.R
+++ NetworkAnalyticsProject/app.R
@@ -26,7 +26,7 @@ ui <- fluidPage(theme = shinytheme("yeti"),
                                      c("Year" = "chart.year",
                                        "Category" = "chart.category",
                                        "Author" = "chart.author",
-                                       "Rating" = "chart.rating")),
+                                       "Book" = "chart.book")),
                         sliderInput("top.n.values",
                                     h4("Top N"),
                                     value = 5,
@@ -75,16 +75,20 @@ server <- function(input, output, session) {
           if(input$bar.chart == "chart.year" && input$generaltab == "Statistics"){
             list(
               h3(paste0("Number of Books: ", get.books.count.year.range(input$year.range))),
-              renderPlot(plot.books.published.by.year(input$year.range))
+              renderPlot(plot.books.published.by.year(input$year.range)),
+              h3(paste0("Number of Distinct Authors: ", get.authors.count.year.range(input$year.range))),
+              renderPlot(plot.distinct.authors.by.year(input$year.range))
             )
           },
           if(input$bar.chart == "chart.author" && input$generaltab == "Statistics"){
             list(
+              h3("Number of Authors per Book Distribution: "),
+              renderPlot(plot.books.by.authornumber(input$year.range)),
               h3(paste0(input$top.n.values, " Authors With Most Books")),
               renderTable(get.authors.most.books(input$top.n.values, input$year.range))
             )
           },
-          if(input$bar.chart == "chart.rating" && input$generaltab == "Statistics"){
+          if(input$bar.chart == "chart.book" && input$generaltab == "Statistics"){
             list(
               h3(paste0("Average Book Rating: ", get.average.book.rating(input$year.range))),
               renderPlot(plot.books.by.ranking(input$year.range)),
@@ -94,11 +98,19 @@ server <- function(input, output, session) {
           },
           if(input$bar.chart == "chart.category" && input$generaltab == "Statistics"){
             list(
-              h3(paste0(input$top.n.values," Categories With Most Books")),
-              renderTable(get.categories.most.books(input$top.n.values, input$year.range))
+              h3(paste0(input$top.n.values,"Categories With Most Books")),
+              renderTable(get.categories.most.books(input$top.n.values, input$year.range)),
+              h3("Distinct Authors for each Category"),
+              renderTable(get.distinct.author.per.category(input$year.range))
             )
           }
         )
+      } else {
+        if(input$maintabs == "Author") {
+          list(
+            h3(paste("Average Book Rating for Author Selected:", author.avg.rank(input$author.name, input$year.range)))
+          )
+        }
       }
     )
     
--- NetworkAnalyticsProject/global.R
+++ NetworkAnalyticsProject/global.R
@@ -4,8 +4,9 @@ library(ggplot2)
 library(shiny)
 library(dplyr)
 
-author.avg.rank <- function(author.name) {
-  dt.author <- dt.books[dt.books$authors == author.name,]
+author.avg.rank <- function(author.name, year.range) {
+  dt.books.range <- filter(dt.books, published_year >= min(year.range) & published_year <= max(year.range))
+  dt.author <- dt.books.range[dt.books.range$authors == author.name,]
   average_rating_author <- mean(dt.author$average_rating)
   round(average_rating_author,3)
 }
@@ -41,14 +42,41 @@ get.books.count.year.range <- function(year.range) {
   length(unique(dt.books.range$title))
 }
 
+get.authors.count.year.range <- function(year.range) {
+  dt.books.range <- filter(dt.books, published_year >= min(year.range) & published_year <= max(year.range))
+  length(unique(dt.books.range$authors))
+}
+
+get.distinct.author.per.category <- function(year.range) {
+  dt.books.range <- filter(dt.books, published_year >= min(year.range) & published_year <= max(year.range))
+  dt.category.number <- aggregate(data = dt.books.range,
+                            authors ~ categories,
+                            function(authors) length(unique(authors)))
+  dt.category.number[order(-dt.category.number$authors),]
+}
+
 plot.books.published.by.year <- function(year.range) {
   dt.books.range <- filter(dt.books, published_year >= min(year.range) & published_year <= max(year.range))
   ggplot(dt.books.range, aes(x=published_year)) + geom_bar() + ggtitle('Books per Year')
 }
 
+plot.distinct.authors.by.year <- function(year.range) {
+  dt.books.range <- filter(dt.books, published_year >= min(year.range) & published_year <= max(year.range))
+  dt.author.number <- aggregate(data = dt.books.range,
+                                  authors ~ published_year,
+                                  function(authors) length(unique(authors)))
+  ggplot(dt.author.number, aes(x=published_year, y=authors)) + geom_bar(stat="identity") + ggtitle('Distinct Authors per Year')
+}
+
 plot.books.by.ranking <- function(year.range) {
   dt.books.range <- filter(dt.books, published_year >= min(year.range) & published_year <= max(year.range))
   ggplot(dt.books.range,aes(x=average_rating)) + geom_histogram() +  ggtitle('Books per Ranking')
+}
+
+plot.books.by.authornumber <- function(year.range) {
+  dt.books.range <- filter(dt.books, published_year >= min(year.range) & published_year <= max(year.range))
+  dt.authors.number <- dt.books.range %>% count(title, sort = TRUE)
+  ggplot(dt.authors.number,aes(x=n)) + geom_histogram() +  ggtitle('Books per Number of Authors')
 }
 
 load("books.RData")
