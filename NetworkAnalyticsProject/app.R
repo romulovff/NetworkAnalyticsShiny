19b42665a3ee8b5e10f73f9ba2f638f5a990d9d3
romulovff <romulo.v.f.f@gmail.com>
2022-03-16 20:07:52 +0000

    Fixed UI issues

--- NetworkAnalyticsProject/app.R
+++ NetworkAnalyticsProject/app.R
@@ -14,6 +14,7 @@ library(shiny)
 library(shinythemes)
 library(hash)
 library(shinyWidgets)
+library(shinydashboard)
 
 
 # Define UI
@@ -65,7 +66,7 @@ ui <- dashboardPage(
         )
       )
     )
-    ),
+  ),
   dashboardBody(
     tabItems(
       tabItem(tabName = "aboutme",
@@ -84,10 +85,11 @@ ui <- dashboardPage(
               ")
       ),
       tabItem(tabName = "statistics",
-              h1("Hello")),
+              uiOutput("general.statistics")),
       tabItem(tabName = "graph",
-              h1("Hello graph")),
-      tabItem(tabName = "author")
+              uiOutput("general.graph")),
+      tabItem(tabName = "author",
+              uiOutput("author"))
     )
   )
 )
@@ -99,83 +101,79 @@ server <- function(input, output, session) {
     paste("Average Book Rating for Author Selected:", author.avg.rank(input$author.name))
   })
   
-  output$out <- renderUI(
-    if(input$maintabs == "General"){
-      if(input$generaltab == "Statistics"){
+  output$general.statistics <- renderUI(
+    list(
+      if(input$bar.chart == "chart.year"){
         list(
-          if(input$bar.chart == "chart.year"){
-            list(
-              h3(paste0("Number of Books: ", get.books.count.year.range(input$year.range))),
-              renderPlot(plot.books.published.by.year(input$year.range)),
-              h3(paste0("Number of Distinct Authors: ", get.authors.count.year.range(input$year.range))),
-              renderPlot(plot.distinct.authors.by.year(input$year.range))
-            )
-          },
-          if(input$bar.chart == "chart.author"){
-            list(
-              h3("Number of Authors per Book Distribution: "),
-              renderPlot(plot.books.by.authornumber(input$year.range)),
-              h3(paste0(input$top.n.values, " Authors With Most Books")),
-              renderTable(get.authors.most.books(input$top.n.values, input$year.range))
-            )
-          },
-          if(input$bar.chart == "chart.book"){
-            list(
-              h3(paste0("Average Book Rating: ", get.average.book.rating(input$year.range))),
-              renderPlot(plot.books.by.ranking(input$year.range)),
-              h3(paste0(input$top.n.values, " Books With Highest Rating")),
-              renderTable(get.books.highest.rating(input$top.n.values, input$year.range))
-            )
-          },
-          if(input$bar.chart == "chart.category"){
-            list(
-              h3(paste0(input$top.n.values,"Categories With Most Books")),
-              renderTable(get.categories.most.books(input$top.n.values, input$year.range)),
-              h3("Distinct Authors for each Category"),
-              renderTable(get.distinct.author.per.category(input$year.range))
-            )
-          }
+          h3(paste0("Number of Books: ", get.books.count.year.range(input$year.range))),
+          renderPlot(plot.books.published.by.year(input$year.range)),
+          h3(paste0("Number of Distinct Authors: ", get.authors.count.year.range(input$year.range))),
+          renderPlot(plot.distinct.authors.by.year(input$year.range))
         )
-      } else {
-        if(input$generaltab == "Graph") {
-          list(
-            h3(print("Authors connected if books written are of the same category")),
-            renderPlot(plot.similar.category.network(input$year.range, input$top.n.values, input$switch.value)),
-            if(input$switch.value == TRUE) {
-              renderPrint(plot.similar.category.network(input$year.range, input$top.n.values, input$switch.value))
-            },
-            h3(print("Authors connected if similar rating")),
-            renderPlot(plot.similar.rating.network(input$year.range, input$top.n.values, input$switch.value)),
-            if(input$switch.value == TRUE) {
-              renderPrint(plot.similar.rating.network(input$year.range, input$top.n.values, input$switch.value))
-            },
-            h3(print("Authors connected if co-written a book")),
-            renderPlot(plot.co.authors.network(input$year.range, input$top.n.values, input$switch.value)),
-            if(input$switch.value == TRUE) {
-              renderPrint(plot.co.authors.network(input$year.range, input$top.n.values, input$switch.value))
-            }
-          )
-        }
-      }
-    } else {
-      if(input$maintabs == "Author") {
+      },
+      if(input$bar.chart == "chart.author"){
         list(
-          # h3(paste("Average Book Rating for Author Selected:", author.avg.rank(input$author.name, input$year.range))),
-          list(
-            h3("Graph that connects author with the book"),
-            renderPlot(plot.author.to.books.network(input$author.name, input$year.range)),
-            h3("Graph that connects author with categories"),
-            renderPlot(plot.author.to.categories.network(input$author.name, input$year.range)),
-            h3("Graph that connect authors with similar rating"),
-            renderPlot(plot.similar.rank.authors(input$author.name, input$year.range, input$top.n.values)),
-            h3("Graph that connect authors with similar category"),
-            renderPlot(plot.similar.category.authors(input$author.name, input$year.range, input$top.n.values)),
-            h3("Graph that connects authors with similar rating and category"),
-            renderPlot(plot.similar.rank.category.authors(input$author.name, input$year.range, input$top.n.values)) # Discuss removal
-          )
+          h3("Number of Authors per Book Distribution: "),
+          renderPlot(plot.books.by.authornumber(input$year.range)),
+          h3(paste0(input$top.n.values, " Authors With Most Books")),
+          renderTable(get.authors.most.books(input$top.n.values, input$year.range))
+        )
+      },
+      if(input$bar.chart == "chart.book"){
+        list(
+          h3(paste0("Average Book Rating: ", get.average.book.rating(input$year.range))),
+          renderPlot(plot.books.by.ranking(input$year.range)),
+          h3(paste0(input$top.n.values, " Books With Highest Rating")),
+          renderTable(get.books.highest.rating(input$top.n.values, input$year.range))
+        )
+      },
+      if(input$bar.chart == "chart.category"){
+        list(
+          h3(paste0(input$top.n.values,"Categories With Most Books")),
+          renderTable(get.categories.most.books(input$top.n.values, input$year.range)),
+          h3("Distinct Authors for each Category"),
+          renderTable(get.distinct.author.per.category(input$year.range))
         )
       }
-    }
+    )
+  )
+  
+  output$general.graph <- renderUI(
+    list(
+      h3(print("Authors connected if books written are of the same category")),
+      renderPlot(plot.similar.category.network(input$year.range, input$top.n.values, input$switch.value)),
+      if(input$switch.value == TRUE) {
+        renderPrint(plot.similar.category.network(input$year.range, input$top.n.values, input$switch.value))
+      },
+      h3(print("Authors connected if similar rating")),
+      renderPlot(plot.similar.rating.network(input$year.range, input$top.n.values, input$switch.value)),
+      if(input$switch.value == TRUE) {
+        renderPrint(plot.similar.rating.network(input$year.range, input$top.n.values, input$switch.value))
+      },
+      h3(print("Authors connected if co-written a book")),
+      renderPlot(plot.co.authors.network(input$year.range, input$top.n.values, input$switch.value)),
+      if(input$switch.value == TRUE) {
+        renderPrint(plot.co.authors.network(input$year.range, input$top.n.values, input$switch.value))
+      }
+    )
+  )
+  
+  output$author <- renderUI(
+    list(
+      # h3(paste("Average Book Rating for Author Selected:", author.avg.rank(input$author.name, input$year.range))),
+      list(
+        h3("Graph that connects author with the book"),
+        renderPlot(plot.author.to.books.network(input$author.name, input$year.range)),
+        h3("Graph that connects author with categories"),
+        renderPlot(plot.author.to.categories.network(input$author.name, input$year.range)),
+        h3("Graph that connect authors with similar rating"),
+        renderPlot(plot.similar.rank.authors(input$author.name, input$year.range, input$top.n.values)),
+        h3("Graph that connect authors with similar category"),
+        renderPlot(plot.similar.category.authors(input$author.name, input$year.range, input$top.n.values)),
+        h3("Graph that connects authors with similar rating and category"),
+        renderPlot(plot.similar.rank.category.authors(input$author.name, input$year.range, input$top.n.values)) # Discuss removal
+      )
+    )
   )
   
   updateSelectInput(
